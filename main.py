5b719dc526c687b99a8b502eeeb8e1b7bb715981
Itz Merml <jeremy-andreas.schierling@stud.uni-bamberg.de>
2025-01-13 19:59:27 +0100

    Add Backend for live logging

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, logging, render_template, request, jsonify
+from flask import Flask, Response, logging, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import os
@@ -319,6 +319,31 @@ def compare_experiments():
     logging.info("Compared experiments.")
     return jsonify(result), 200
 
+#Live Logging for Output 
+logs = {}
+
+@app.route('/experiments/<string:run_id>/logs', methods = ['POST'])
+def get_experiment_logs(run_id): 
+    #Get logs from workstation and save them temporarily
+    data = request.json
+    log_entry = data.get('log')
+    if not log_entry: 
+        return jsonify({"error": "No log entry provided."}), 400
+    
+    if run_id not in logs: 
+        logs[run_id] = []
+    logs[run_id].append(log_entry)
+    
+    return jsonify({"message": "Log received successfully."}), 200
+
+@app.route('/experiments/<string:run_id>/logs', methods = ['GET'])
+def stream_logs(run_id):
+    def generate():
+        if run_id in logs:
+            for log in logs[run_id]:
+                yield log + '\n'
+    
+    return Response(generate(), mimetype='text/plain')
 
 @app.route('/')
 def hello():
